cede67745a525ca44b5d9a4b8ee98775e3dfb000
tweiiy <tweiiymail@gmail.com>
2022-07-18 19:28:39 +0300

    sidebar section heading

---
+++ src/components/SidebarSectionHeading.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-sm font-semibold px-6 py-2 uppercase">
+    {{title}}
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    title:String
+  }
+}
+</script>
--- src/components/TheSidebarBig.vue
+++ src/components/TheSidebarBig.vue
@@ -17,18 +17,11 @@
       <span class="text-sm pb-2"> <!-- текст регистрации -->
         Sign in to like videos, comment, and subscribe.
       </span>
-      <ButtonLogin />
-      <!-- <button class="flex items-center px-2 py-0.5 mt-3 text-sm text-blue-700 uppercase border border-blue-700 rounded-sm">
-        <BaseIcon name="login" class="mr-2 h-7 w-7" />
-        <span>
-          Sing in
-        </span>
-      </button> -->
+      <ButtonLogin /> <!-- логин -->
     </section>
     <section class="py-2 border-b"> <!-- 3 список -->
-      <div class="text-sm font-semibold px-6 py-2">
-        BEST OF YOUTUBE
-      </div>
+      <SidebarSectionHeading title="best of youtube" />
+      
       <ul>
         <SidebarNavItem label="Music" icon="note" is-black />
         <SidebarNavItem label="Sports" icon="gift" is-black />
@@ -43,9 +36,7 @@
     <SidebarNavItem label="Browse channels" icon="plus" />
     </section>
     <section class="py-2 border-b"> <!-- MORE FROM YOUTUBE -->
-      <div class="text-sm font-semibold px-6 py-2">
-        MORE FROM YOUTUBE
-      </div>
+      <SidebarSectionHeading title="more from youtube" />
       <ul>
         <SidebarNavItem label="YouTube Premium" icon="play" />
         <SidebarNavItem label="Live" icon="signal" />
@@ -60,21 +51,21 @@
       </ul>
     </section>
     <section class="px-6 py-2 text-xs font-semibold space-y-4"> <!-- подвал -->
-      <div class="mr-2"> <!-- 1 текст -->
-        <a href="">About</a>
-        <a href="">Press</a>
-        <a href="">Copyright</a>
-        <a href="">Contact us</a>
-        <a href="">Creators</a>
-        <a href="">Advertise</a>
-        <a href="">Developers</a>
+      <div class="space-x-2"> <!-- 1 текст -->
+        <a class="" href="#">About</a>
+        <a class="" href="#">Press</a>
+        <a class="" href="#">Copyright</a>
+        <a class="" href="#">Contact us</a>
+        <a class="" href="#">Creators</a>
+        <a class="" href="#">Advertise</a>
+        <a class="" href="#">Developers</a>
       </div>
-      <div class="mr-2"> <!-- 2 текст -->
-        <a href="">Trems</a>
-        <a href="">Privacy</a>
-        <a href="">Policy & Safety</a>
-        <a href="">How YouTube works</a>
-        <a href="">Test new features</a>
+      <div class="space-x-2"> <!-- 2 текст -->
+        <a class="" href="#">Trems</a>
+        <a class="" href="#">Privacy</a>
+        <a class="" href="#">Policy & Safety</a>
+        <a class="" href="#">How YouTube works</a>
+        <a class="" href="#">Test new features</a>
       </div>
       <div  class="font-normal text-gray-500"> <!-- копирайт -->
         © 2022 Google LLC
@@ -87,12 +78,14 @@
 import SidebarNavItem from './SidebarNavItem.vue'
 import BaseIcon from './BaseIcon.vue'
 import ButtonLogin from './ButtonLogin.vue'
+import SidebarSectionHeading from './SidebarSectionHeading.vue'
 
 export default {
   components:{
     SidebarNavItem,
     BaseIcon,
-    ButtonLogin
+    ButtonLogin,
+    SidebarSectionHeading
   }
 }
 </script>
